ae3612612519b17f4b76b2f98bcc0dc2537ab504
RichardJeanBaptiste <Richinbk1@gmail.com>
2020-01-07 19:29:06 -0500

    added cli language

--- TTS.py
+++ TTS.py
@@ -3,14 +3,16 @@ import sys
 from gtts import gTTS
 
 mytext = ""
-language = 'en'
+language = sys.argv[2]
+fileName = sys.argv[3]
 
 with open(sys.argv[1], 'r') as f:
    mytext = f.read()
 
 myobj = gTTS(text=mytext, lang = language, slow=False)
 
-myobj.save("test.mp3")
+myobj.save(fileName)
+
 
 
 
